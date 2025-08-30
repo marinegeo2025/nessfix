893ba51ea243e0f1d0350bbfbb1e0c9568de6d79
marinegeo2025 <barnardalex@gmail.com>
2025-08-30 22:53:19 +0100

    Update card.png.js

--- api/card.png.js
+++ api/card.png.js
@@ -1,20 +1,30 @@
 // api/card.png.js
-import { scrapeLHFA } from "./_scrape.js";
-import { buildSVG } from "./_card-template.js";
-import { Resvg } from "@resvg/resvg-js";
+ import { scrapeLHFA } from "./_scrape.js";
+ import { buildSVG } from "./_card-template.js";
+ import { Resvg } from "@resvg/resvg-js";
++import fs from "fs";
 
-export default async function handler(req, res) {
-  try {
-    const data = await scrapeLHFA();
-    const svg = buildSVG(data);
+ export default async function handler(req, res) {
+   try {
+     const data = await scrapeLHFA();
+     const svg = buildSVG(data);
 
-    const r = new Resvg(svg, {
-     fitTo: { mode: "width", value: 1200 },
-     textRendering: 1,
-     font: { loadSystemFonts: true }   // <-- allow fallback fonts if present
-   });
+-    const r = new Resvg(svg, {
+-     fitTo: { mode: "width", value: 1200 },
+-     textRendering: 1,
+-     font: { loadSystemFonts: true }   // <-- allow fallback fonts if present
+-   });
++    // Load bundled fonts
++    const fontRegular = fs.readFileSync("./fonts/GeogrotesqueCompTRIAL-Rg.otf");
++    const fontBold    = fs.readFileSync("./fonts/GeogrotesqueCondTRIAL-Bd.otf");
++
++    const r = new Resvg(svg, {
++      fitTo: { mode: "width", value: 1200 },
++      textRendering: 1,
++      font: { fontFiles: [fontRegular, fontBold] }
++    });
 
-    const png = r.render().asPng();
+     const png = r.render().asPng();
 
     res.setHeader("Content-Type", "image/png");
     res.setHeader("Cache-Control", "no-store, must-revalidate");
