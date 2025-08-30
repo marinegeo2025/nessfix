cf995eca9ca4bffb048189241b11c3ca2016fd0b
marinegeo2025 <barnardalex@gmail.com>
2025-08-30 22:56:51 +0100

    Update card.png.js

--- api/card.png.js
+++ api/card.png.js
@@ -1,30 +1,32 @@
 // api/card.png.js
- import { scrapeLHFA } from "./_scrape.js";
- import { buildSVG } from "./_card-template.js";
- import { Resvg } from "@resvg/resvg-js";
-+import fs from "fs";
+import { scrapeLHFA } from "./_scrape.js";
+import { buildSVG } from "./_card-template.js";
+import { Resvg } from "@resvg/resvg-js";
+import fs from "fs";
 
- export default async function handler(req, res) {
-   try {
-     const data = await scrapeLHFA();
-     const svg = buildSVG(data);
+export default async function handler(req, res) {
+  try {
+    const data = await scrapeLHFA();
+    const svg = buildSVG(data);
 
--    const r = new Resvg(svg, {
--     fitTo: { mode: "width", value: 1200 },
--     textRendering: 1,
--     font: { loadSystemFonts: true }   // <-- allow fallback fonts if present
--   });
-+    // Load bundled fonts
-+    const fontRegular = fs.readFileSync("./fonts/GeogrotesqueCompTRIAL-Rg.otf");
-+    const fontBold    = fs.readFileSync("./fonts/GeogrotesqueCondTRIAL-Bd.otf");
-+
-+    const r = new Resvg(svg, {
-+      fitTo: { mode: "width", value: 1200 },
-+      textRendering: 1,
-+      font: { fontFiles: [fontRegular, fontBold] }
-+    });
+    // Load your bundled fonts (OTF is fine)
+    const fontRegular = fs.readFileSync("./fonts/GeogrotesqueCompTRIAL-Rg.otf");
+    const fontBold    = fs.readFileSync("./fonts/GeogrotesqueCondTRIAL-Bd.otf");
 
-     const png = r.render().asPng();
+    const r = new Resvg(svg, {
+      fitTo: { mode: "width", value: 1200 },
+      textRendering: 1,
+      font: {
+        // Tell resvg which family name to expose to the SVG
+        fontFiles: [
+          { file: fontRegular, family: "Geogrotesque", weight: 400 },
+          { file: fontBold,    family: "Geogrotesque", weight: 700 }
+        ],
+        defaultFontFamily: "Geogrotesque"
+      }
+    });
+
+    const png = r.render().asPng();
 
     res.setHeader("Content-Type", "image/png");
     res.setHeader("Cache-Control", "no-store, must-revalidate");
